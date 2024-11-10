f051b77ac62704729e46e1d5399864c646d243f2
Happy Gopher <max@happygopher.nl>
2024-11-09 16:47:00 -0800

    set up vscode

--- hosts/users/parallels/shared/vscode.nix
+++ hosts/users/parallels/shared/vscode.nix
@@ -5,41 +5,40 @@
     mutableExtensionsDir = false;
     enableUpdateCheck = false;
     enableExtensionUpdateCheck = false;
-    
+
+    # Nix vscode extensions
     extensions = with pkgs.vscode-extensions; [
-      # Core extensions available in nixpkgs
-      bbenoist.nix
-      eamodio.gitlens
-      #github.copilot
-      golang.go
-      rust-lang.rust-analyzer
-      ms-azuretools.vscode-docker
-      vadimcn.vscode-lldb
-      tamasfe.even-better-toml
+      # Theme and UI
+      emroussel.atomize-atom-one-dark-theme  # Color theme
+      pkief.material-icon-theme              # File icons
+      usernamehw.errorlens                   # Inline error display
 
-      pkgs.vscode-extensions.mskelton.one-dark-theme
-      pkgs.vscode-extensions.pkief.material-icon-theme
+      # Language Support
+      bbenoist.nix                           # Nix language
+      golang.go                              # Go language
+      #rust-lang.rust-analyzer               # Rust language (commented out)
+      tamasfe.even-better-toml               # TOML support
 
-      #pkgs.vscode-extensions.equinusocio.vsc-material-theme
-      #pkgs.vscode-extensions.equinusocio.vsc-material-theme-icons
+      # Development Tools
+      eamodio.gitlens                       # Git integration
+      ms-azuretools.vscode-docker           # Docker support
+      formulahendry.auto-close-tag          # HTML/XML tag closing
+      ms-vscode.makefile-tools              # Makefile support
 
-      #pkgs.vscode-extensions.ms-vscode-remote.remote-containers
-      #pkgs.vscode-extensions.github.vscode-github-actions
-      #pkgs.vscode-extensions.ritwickdey.liveserver
-      pkgs.vscode-extensions.usernamehw.errorlens
-      pkgs.vscode-extensions.formulahendry.auto-close-tag
-      pkgs.vscode-extensions.ms-vscode.makefile-tools
-
-      pkgs.vscode-extensions.ms-vscode.test-adapter-converter
-      pkgs.vscode-extensions.hbenl.vscode-test-explorer
-      
+      # Testing and Debugging
+      #vadimcn.vscode-lldb                  # LLDB debugger (cause warnings in settings.json)
+      ms-vscode.test-adapter-converter      # Test adapter support
+      hbenl.vscode-test-explorer            # Test explorer UI
     ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      # Additional Tools
       {
         name = "dependi";
         publisher = "fill-labs";
         version = "0.7.10";
         sha256 = "m8W21ztTmEOjDI1KCymeBgQzg9jdgKG9dCFp+U1D818=";
       }
+
+      #Rust Tools
       {
         name = "rust-test-lens";
         publisher = "hdevalke";
@@ -59,91 +58,74 @@
         sha256 = "IgfcIRF54JXm9l2vVjf7lFJOVSI0CDgDjQT+Hw6FO4Q=";
       }
     ];
-    
+
     userSettings = {
-      # Update settings
+      # update settings\
       "update.mode" = "none";
-      "extensions.autoUpdate" = false;
       "extensions.autoCheckUpdates" = false;
 
+      # theme settings
+      "workbench.colorTheme" = "Atomize";
+
       # Editor settings
-      "editor.fontFamily" = "'JetBrainsMono', 'FiraCode Nerd Font', monospace";
-      "editor.fontLigatures" = true;
-      "editor.fontSize" = 20;
-      "editor.lineHeight" = 30;
+      "editor" = {
+        "fontFamily" = "'JetBrainsMono NF', 'FiraCode Nerd Font', monospace";
+        "fontLigatures" = true;
+        "fontSize" = 20;
+        "lineHeight" = 30;
+      };
 
       # Terminal settings
-      "terminal.integrated.fontSize" = 18;
-      "terminal.integrated.fontFamily" = "'JetBrainsMono Nerd Font'";
+      "terminal.integrated" = {
+        "fontFamily" = "'JetBrainsMono NF', 'FiraCode Nerd Font', monospace";
+        "fontSize" = 20;
+      };
 
-      # Go specific settings
+      # Language-specific settings
       "[go]" = {
-        "editor.formatOnSave" = true;
         "editor.codeActionsOnSave" = {
           "source.organizeImports" = "explicit";
         };
+        "editor.formatOnSave" = true;
         "editor.snippetSuggestions" = "none";
       };
 
-      "go.useLanguageServer" = true;
-      "go.toolsManagement.autoUpdate" = false;
-      "go.lintTool" = "golangci-lint";
-      "go.testOnSave" = false;
-      "go.coverOnSave" = false;
-      "go.coverOnSingleTest" = true;
+      # Go settings
+      "go" = {
+        "coverOnSave" = false;
+        "coverOnSingleTest" = true;
+        "delveConfig" = {
+          "debugAdapter" = "dlv-dap";
+          "showGlobalVariables" = true;
+        };
+        "lintTool" = "golangci-lint";
+        "testOnSave" = false;
+        "toolsManagement.autoUpdate" = false;
+        "useLanguageServer" = true;
+      };
 
-      # Debugging settings for Go
+      # Go debugger settings
       "go.delveConfig" = {
-        "debugAdapter" = "dlv-dap";  # Use the DAP protocol
+        "debugAdapter" = "dlv-dap";
         "showGlobalVariables" = true;
       };
 
-      # Theme settings
-      "workbench.colorTheme" = "One Dark";
-      "workbench.iconTheme" = "material-icon-theme";
-      "workbench.colorCustomizations" = {
-        "editorError.border" = "#fff0";
-        "editorError.foreground" = "#fff0";
-        "editorWarning.border" = "#fff0";
-        "editorWarning.foreground" = "#fff0";
-     };
-      "editor.tokenColorCustomizations" = {
-        "textMateRules" = [
-          {
-            "name" = "One Dark italic";
-            "scope" = [
-              "comment"
-              "entity.other.attribute-name"
-              "keyword"
-              "markup.underline.link"
-              "storage.modifier"
-              "storage.type"
-              "string.url"
-              "variable.language.super"
-              "variable.language.this"
-            ];
-            "settings" = {
-              "fontStyle" = "italic";
-            };
-          }
-          {
-            "name" = "One Dark italic reset";
-            "scope" = [
-              "keyword.operator"
-              "keyword.other.type"
-              "storage.modifier.import"
-              "storage.modifier.package"
-              "storage.type.built-in"
-              "storage.type.function.arrow"
-              "storage.type.generic"
-              "storage.type.java"
-              "storage.type.primitive"
-            ];
-            "settings" = {
-              "fontStyle" = "";
-            };
-          }
-        ];
+      # Debug settings
+      "debug" = {
+        "console" = {
+          "fontFamily" = "'JetBrainsMono NF', 'FiraCode Nerd Font', monospace";
+          "fontSize" = 20;
+        };
+      };
+
+      # LLDB specific settings
+      "lldb" = {
+        "commandCompletions" = true;
+        "evaluateForHovers" = true;
+        "launch" = {
+          "terminal" = "integrated";
+        };
+        "suppressMissingSourceFiles" = true;
       };
     };
   };
