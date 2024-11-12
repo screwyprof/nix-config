5a41e41318b17d149f6d71c97d46c475abb8f511
Happy Gopher <max@happygopher.nl>
2024-11-11 23:18:19 -0800

    neofetch gopher

--- home/modules/shared/core/neofetch.nix
+++ home/modules/shared/core/neofetch.nix
@@ -3,8 +3,26 @@
     neofetch
   ];
 
+  # First create the gopher ASCII art file
+  xdg.configFile."neofetch/gopher.txt".text = ''
+             ,_---~~~~~----._         
+      _,,_,*^____      _____``*g*\"*, 
+     / __/ /'     ^.  /      \ ^@q   f 
+    [  @f | @))    |  | @))   l  0 _/  
+     \`/   \~____ / __ \_____/    \   
+      |           _l__l_           I   
+      }          [______]           I  
+      ]            | | |            |  
+      ]             ~ ~             |  
+      |                            |   
+       |                           |   
+  '';
+
+  # Then reference it in the config
   xdg.configFile."neofetch/config.conf".text = ''
     print_info() {
+        prin "$(color 4)"
+     
         info title
         info underline
         
@@ -23,44 +41,19 @@
         info "󰆍" term
         
         prin "$(color 4)╭──────────── System ─────────────"
-        prin "$(color 4)󰋊" "$(df -h / | awk 'NR==2 {printf "%s used of %s (%s)", $3, $2, $5}')"
-        prin "$(color 4)󰅐" "$(uptime | cut -d',' -f1 | awk '{split($3,a,":"); if (a[1] >= 24) printf "%d days, %d hours, %d mins", a[1]/24, a[1]%24, a[2]; else if (a[1] > 0) printf "%d hours, %d mins", a[1], a[2]; else printf "%d mins", a[2]}')"
+        prin "$(color 4)󰋊 Disk   " "$(df -h / | awk 'NR==2 {printf "%s used of %s (%s)", $3, $2, $5}')"
+        prin "$(color 4)󰅐 Uptime " "$(uptime | cut -d',' -f1 | awk '{split($3,a,":"); if (a[1] >= 24) printf "%d days, %d hours, %d mins", a[1]/24, a[1]%24, a[2]; else if (a[1] > 0) printf "%d hours, %d mins", a[1], a[2]; else printf "%d mins", a[2]}')"
         prin "$(color 4)───────────────────────────────────"
     }
     
     # Colors
-    colors=(4 4 4 4 4 7)  # Changed to blue theme
+    colors=(4 4 4 4 4 7)
     
-    # Text Options
-    bold="on"
-    underline_enabled="on"
-    underline_char="-"
-    separator="  "        # Using two spaces for better alignment
-    
-    # CPU
-    cpu_brand="on"
-    cpu_speed="on"
-    cpu_cores="logical"
-    
-    # Memory
-    memory_percent="on"
-    memory_unit="gib"
-    
-    # Packages
-    package_managers="on"
-    
-    # Shell
-    shell_path="off"
-    shell_version="on"
-    
-    # OS Info
-    os_arch="on"
-    distro_shorthand="on"
-
     # Backend Settings
     image_backend="ascii"
-    ascii_distro="Darwin"
-    ascii_colors=(1 2 3 4 5 6)
+    image_source="${config.xdg.configHome}/neofetch/gopher.txt"
+    ascii_distro="none"
+    ascii_colors=(4 4 4 4 4 7)
     ascii_bold="on"
   '';
 }
