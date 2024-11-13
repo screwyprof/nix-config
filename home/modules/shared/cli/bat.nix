e19a9b25d7686a4e7d16c5e25cc15e6a8729d41d
Happy Gopher <max@happygopher.nl>
2024-11-12 21:45:10 -0800

    fix bat

--- home/modules/shared/cli/bat.nix
+++ home/modules/shared/cli/bat.nix
@@ -22,35 +22,34 @@
     prettybat
   ];
 
-  programs.zsh {
-  shellAliases = {
-    # Plain cat replacement (no styling, no paging)
-    cat = "bat --plain --paging=never";
+  programs.zsh = {
+    shellAliases = {
+      # Plain cat replacement (no styling, no paging)
+      cat = "bat --plain --paging=never";
 
-    # Last 80 lines of history, newest first
-    history = "history 0 | tail -n 80 | tac | awk '{$1=\"\"; print substr($0,2)}' | bat --file-name 'Shell History' --language=bash";
+      # Last 80 lines of history, newest first
+      history = "history 0 | tail -n 80 | tac | awk '{$1=\"\"; print substr($0,2)}' | bat --file-name 'Shell History' --language=bash";
 
-    # Other aliases
-    diff = "${pkgs.bat-extras.batdiff}/bin/batdiff";
-    rg = "${pkgs.ripgrep}/bin/rg --hidden --glob '!.git'";
+      # Other aliases
+      diff = "${pkgs.bat-extras.batdiff}/bin/batdiff";
+      rg = "${pkgs.ripgrep}/bin/rg --hidden --glob '!.git'";
 
-    # Bat-extras aliases
-    batdiff = "${pkgs.bat-extras.batdiff}/bin/batdiff";
-    batgrep = "${pkgs.bat-extras.batgrep}/bin/batgrep";
-    batman = "${pkgs.bat-extras.batman}/bin/batman";
-    prettybat = "${pkgs.bat-extras.prettybat}/bin/prettybat";
+      # Bat-extras aliases
+      batdiff = "${pkgs.bat-extras.batdiff}/bin/batdiff";
+      batgrep = "${pkgs.bat-extras.batgrep}/bin/batgrep";
+      batman = "${pkgs.bat-extras.batman}/bin/batman";
+      prettybat = "${pkgs.bat-extras.prettybat}/bin/prettybat";
+    };
+
+    initExtra = ''
+      # Enhanced tail -f with bat
+      tail() {
+        if [[ "$1" == "-f" ]]; then
+          command tail "$@" | bat --paging=never -l log
+        else
+          command tail "$@"
+        fi
+      }
+    '';
   };
-
-  initBatFunctions = ''
-    # Enhanced tail -f with bat
-    tail() {
-      if [[ "$1" == "-f" ]]; then
-        command tail "$@" | bat --paging=never -l log
-      else
-        command tail "$@"
-      fi
-    }
-  '';
-}
-
 }
