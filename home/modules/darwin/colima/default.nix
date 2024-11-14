faab30d1755f32eab4c5ac053959eb6235ff71ea
Happy Gopher <max@happygopher.nl>
2024-11-13 18:34:26 -0800

    fix colima

--- home/modules/darwin/colima/default.nix
+++ home/modules/darwin/colima/default.nix
@@ -75,7 +75,9 @@ in
         RunAtLoad = true;
         StandardOutPath = "${paths.logDir}/colima.log";
         StandardErrorPath = "${paths.logDir}/colima.error.log";
-        EnvironmentVariables = envVars;
+        EnvironmentVariables = envVars // {
+          VERBOSE_ARG = "--verbose";  # Always run daemon in verbose mode
+        };
         KeepAlive = {
           Crashed = true;
           SuccessfulExit = false;
