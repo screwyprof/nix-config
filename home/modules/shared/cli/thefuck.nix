8be1bc379023e87c8915cc3f1f1a21ac80db8111
Maksim Shcherbo <max.shcherbo@consensys.net>
2024-11-15 18:48:40 +0000

    fix thefuck

--- home/modules/shared/cli/thefuck.nix
+++ home/modules/shared/cli/thefuck.nix
@@ -1,14 +1,11 @@
 { config, lib, pkgs, ... }: {
   programs.zsh = {
     initExtra = lib.mkBefore ''
+      # Initialize thefuck with standard mode
       eval "$(thefuck --alias)"
 
+      # Custom function that executes fuck directly
       fuck-command-line() {
-        # Get the failed command
-        local cmd="$(fc -ln -1)"
-        echo "Last command was: $cmd"  # Debug output
-        
-        # Run regular fuck command
         BUFFER="fuck"
         zle accept-line
       }
@@ -18,11 +15,23 @@
     '';
   };
 
+  home.sessionVariables = {
+    # Core settings
+    THEFUCK_REQUIRE_CONFIRMATION = "true";
+    THEFUCK_NO_COLORS = "false";
+    THEFUCK_DEBUG = "false";
+
+    # History settings
+    THEFUCK_HISTORY_LIMIT = "2000";
+    THEFUCK_ALTER_HISTORY = "true";
+
+    # Rules configuration
+    THEFUCK_PRIORITY = "no_command=9999:history=8000:sudo=1000";
+    THEFUCK_RULES = "sudo:no_command:git_pull:git_push:npm_wrong_command";
+    THEFUCK_EXCLUDE_RULES = "git_pull_uncommitted_changes";
+  };
+
   home.packages = with pkgs; [
     thefuck
   ];
-
-  home.sessionVariables = {
-    THEFUCK_REQUIRE_CONFIRMATION = "true";
-  };
 }
