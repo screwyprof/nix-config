7d930b36fd23746c814b7f33a5c3eca8e243a5a8
Happy Gopher <max@happygopher.nl>
2025-11-08 22:16:04 +0000

    move theme helpers into a lib/

--- home/modules/shared/themes/default.nix
+++ home/modules/shared/themes/default.nix
@@ -1,6 +1,8 @@
-{ config, nix-colors, ... }:
+{ config, nix-colors, lib, ... }:
 
 let
+  themeLib = import ./lib { inherit lib; };
+
   presets = import ./presets { inherit nix-colors; };
   activePreset = presets.dracula;
 
@@ -20,8 +22,6 @@ let
     in
     baseImport ++ themeImport;
 
-  # Convert hex string to decimal using TOML parsing
-  hexToDec = hex: (builtins.fromTOML "n = 0x${hex}").n;
 in
 {
   imports =
@@ -33,17 +33,5 @@ in
     ];
 
   colorScheme = activePreset.scheme;
-  lib.theme = {
-    # Convert hex to RGB components
-    hexToRGB = hex: {
-      r = hexToDec (builtins.substring 0 2 hex);
-      g = hexToDec (builtins.substring 2 2 hex);
-      b = hexToDec (builtins.substring 4 2 hex);
-    };
-
-    # Format RGB values for ANSI sequences
-    formatRGB = hex:
-      let rgb = config.lib.theme.hexToRGB hex;
-      in "${toString rgb.r}//${toString rgb.g}//${toString rgb.b}";
-  };
+  lib.theme = themeLib;
 }
---
+++ home/modules/shared/themes/lib/default.nix
@@ -0,0 +1,24 @@
+{ lib }:
+
+let
+  # Convert hex string to decimal using TOML parsing
+  hexToDec = hex: (builtins.fromTOML "n = 0x${hex}").n;
+
+  # Convert hex to RGB components
+  hexToRGB = hex: {
+    r = hexToDec (builtins.substring 0 2 hex);
+    g = hexToDec (builtins.substring 2 2 hex);
+    b = hexToDec (builtins.substring 4 2 hex);
+  };
+
+  # Format RGB values for ANSI sequences
+  formatRGB = hex:
+    let rgb = hexToRGB hex;
+    in "${toString rgb.r}//${toString rgb.g}//${toString rgb.b}";
+in
+{
+  inherit hexToRGB formatRGB;
+
+  # Additional helper functions we might add later
+  inherit (lib) mkOption types; # Re-export common lib functions
+}
