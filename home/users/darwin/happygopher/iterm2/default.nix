41f730f56d16db62ab088dd7caf0901218cec160
Happy Gopher <max@happygopher.nl>
2026-01-30 03:14:50 +0000

    fix iterm2 activation

--- home/users/darwin/happygopher/iterm2/default.nix
+++ home/users/darwin/happygopher/iterm2/default.nix
@@ -12,10 +12,10 @@
         "$HOME/Library/Application Support/iTerm2/DynamicProfiles/happy-gopher.json"
 
       # Set profile as default using defaults command
-      defaults write com.googlecode.iterm2 "Default Bookmark Guid" -string "7CF39BB4-4F21-4FB8-ACD5-3056C304A2C5"
+      run /usr/bin/defaults write com.googlecode.iterm2 "Default Bookmark Guid" -string "7CF39BB4-4F21-4FB8-ACD5-3056C304A2C5"
 
       # Reload iTerm2 settings if it's running
-      if pgrep "iTerm2" > /dev/null; then
+      if /usr/bin/pgrep "iTerm2" > /dev/null; then
         #run killall "iTerm2"
         verboseEcho "iTerm2 is running, please reload manually"
       fi
