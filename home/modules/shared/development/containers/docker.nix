9b4005be769e4479fec147c35aaa44de7a9415e5
Happy Gopher <max@happygopher.nl>
2026-02-09 18:56:09 +0000

    migrate docker-compose to docker compose

--- home/modules/shared/development/containers/docker.nix
+++ home/modules/shared/development/containers/docker.nix
@@ -1,21 +1,21 @@
 { pkgs, lib, ... }: {
   home.packages = with pkgs; [
     docker
-    docker-compose
+    #docker-compose
     docker-credential-helpers
   ];
 
   programs.zsh = {
     shellAliases = {
-      dcp = "docker-compose pull";
-      dcps = "docker-compose ps";
-      dcu = "docker-compose up -d";
-      dcd = "docker-compose down --remove-orphans --volumes";
-      dcr = "docker-compose restart";
-      dclf = "docker-compose logs -f";
+      dcp = "docker compose pull";
+      dcps = "docker compose ps";
+      dcu = "docker compose up -d";
+      dcd = "docker compose down --remove-orphans --volumes";
+      dcr = "docker compose restart";
+      dclf = "docker compose logs -f";
       dlf = "docker logs -f";
-      dcuf = "docker-compose up --build --force-recreate --no-deps -d";
-      dcs = "docker-compose stop";
+      dcuf = "docker compose up --build --force-recreate --no-deps -d";
+      dcs = "docker compose stop";
       drac = "docker container prune";
       drav = "docker volume prune";
       dra = "docker system prune --volumes";
