3419c85e55540d28461f955cea372378b15578ab
Happy Gopher <max@happygopher.nl>
2026-02-09 04:59:08 +0000

    change vscode settings

--- home/modules/shared/development/vscode.nix
+++ home/modules/shared/development/vscode.nix
@@ -15,88 +15,172 @@
           dracula-theme.theme-dracula
           pkief.material-icon-theme
           pkief.material-product-icons
-          usernamehw.errorlens
 
           # Language Support
           jnoortheen.nix-ide
-          golang.go
-          rust-lang.rust-analyzer
-          tamasfe.even-better-toml
-
+          #golang.go
+          #rust-lang.rust-analyzer
           # AI
           #anthropic.claude-code
 
           # c/c++
           #ms-vscode.cpptools-extension-pack
-          ms-vscode.cpptools
-          ms-vscode.cmake-tools
+          #ms-vscode.cpptools
+          #ms-vscode.cmake-tools
 
           # Development Tools
-          fill-labs.dependi
-          eamodio.gitlens
-          ms-azuretools.vscode-docker
+          #usernamehw.errorlens
+          #fill-labs.dependi
+          #eamodio.gitlens
+          #ms-azuretools.vscode-docker
+          tamasfe.even-better-toml
           formulahendry.auto-close-tag
-          ms-vscode.makefile-tools
-          ms-vscode.live-server
+          #ms-vscode.makefile-tools
+          #ms-vscode.live-server
+          #ryanluker.vscode-coverage-gutters
 
           # Testing and Debugging
-          vadimcn.vscode-lldb
+          #vadimcn.vscode-lldb
 
           # Python Support
-          ms-python.python
-          ms-python.vscode-pylance
+          #ms-python.python
+          #ms-python.vscode-pylance
 
           # Remote Development
-          ms-vscode-remote.remote-ssh
+          #ms-vscode-remote.remote-ssh
+
+          # Dev containers
           ms-vscode-remote.remote-containers
         ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
           # Coverage Tools
-          {
-            name = "vscode-coverage-gutters";
-            publisher = "ryanluker";
-            version = "2.12.0";
-            sha256 = "sha256-Dkc/Wqc122fV1r6IUyHOtuRdpbWHL3elAhfxHcY6xtM";
-          }
+          # {
+          #   name = "vscode-coverage-gutters";
+          #   publisher = "ryanluker";
+          #   version = "2.12.0";
+          #   sha256 = "sha256-Dkc/Wqc122fV1r6IUyHOtuRdpbWHL3elAhfxHcY6xtM";
+          # }
 
           # Rust Tools
-          {
-            name = "rust-test-lens";
-            publisher = "hdevalke";
-            version = "1.0.0";
-            sha256 = "faSBpgFIyqA9F03ghizTyFaB+XOwA1pNrStQlLTV9Pk=";
-          }
-          {
-            name = "rust-doc-viewer";
-            publisher = "jscearcy";
-            version = "4.2.0";
-            sha256 = "x1pmrw8wYHWyNIJqVdoh+vasbHDG/A4m8vDZU0DnPzo=";
-          }
+          # {
+          #   name = "rust-test-lens";
+          #   publisher = "hdevalke";
+          #   version = "1.0.0";
+          #   sha256 = "faSBpgFIyqA9F03ghizTyFaB+XOwA1pNrStQlLTV9Pk=";
+          # }
+          # {
+          #   name = "rust-doc-viewer";
+          #   publisher = "jscearcy";
+          #   version = "4.2.0";
+          #   sha256 = "x1pmrw8wYHWyNIJqVdoh+vasbHDG/A4m8vDZU0DnPzo=";
+          # }
         ];
 
         userSettings = {
-          # Language-specific settings
-          "[go]" = {
-            "editor.codeActionsOnSave" = {
-              "source.organizeImports" = "explicit";
+          # A temp hack to avoid errors for extensions which haven't migrated yet
+          # https://code.visualstudio.com/updates/v1_101#_web-environment-detection
+          "extensions.supportNodeGlobalNavigator" = true;
+
+          # Update settings
+          "extensions.autoCheckUpdates" = false;
+          "update.mode" = "none";
+
+          # Disable executing script on project startup
+          "task.allowAutomaticTasks" = "off";
+
+          # Remote settings
+          #"remote.SSH.configFile" = "~/.ssh/config";
+
+          "json.schemaDownload.enable" = false;
+          "window.openFilesInNewWindow" = "on";
+          "window.zoomLevel" = 1;
+
+          # Terminal settings
+          "terminal.integrated.fontFamily" = "'MesloLGMDZ Nerd Font', 'JetBrainsMono NF', 'FiraCode Nerd Font', monospace";
+          "terminal.integrated.fontSize" = 20;
+          "terminal.integrated.defaultProfile.osx" = "zsh";
+          "terminal.integrated.profiles.osx" = {
+            "zsh" = {
+              "path" = "${config.home.homeDirectory}/.nix-profile/bin/zsh";
             };
-            "editor.formatOnSave" = true;
-            "editor.snippetSuggestions" = "none";
           };
 
-          "[python]" = {
-            "editor.codeActionsOnSave" = {
-              "source.organizeImports" = "explicit";
-            };
-            "editor.defaultFormatter" = "ms-python.python";
-            "editor.formatOnSave" = true;
-          };
-          "python.defaultInterpreterPath" = "${pkgs.python312}/bin/python3";
+          # # LLDB settings
+          # "lldb.commandCompletions" = true;
+          # "lldb.evaluateForHovers" = true;
+          # "lldb.launch.terminal" = "integrated";
+          # "lldb.suppressMissingSourceFiles" = true;
 
-          "[rust]" = {
-            "editor.defaultFormatter" = "rust-lang.rust-analyzer";
-            "editor.formatOnSave" = true;
-            "editor.inlayHints.enabled" = "onUnlessPressed";
-          };
+          # # Coverage Settings
+          # "coverage-gutters.coverageFileNames" = [
+          #   "target/coverage/lcov.info"
+          # ];
+          # "coverage-gutters.showGutterCoverage" = true;
+          # "coverage-gutters.showLineCoverage" = true;
+          # "coverage-gutters.showRulerCoverage" = true;
+          # "coverage-gutters.highlightdark" = "rgba(64,128,64,0.4)";
+
+          # # Go settings
+          # "[go]" = {
+          #   "editor.codeActionsOnSave" = {
+          #     "source.organizeImports" = "explicit";
+          #   };
+          #   "editor.formatOnSave" = true;
+          #   "editor.snippetSuggestions" = "none";
+          # };
+          # "go.lintTool" = "golangci-lint";
+          # "go.testOnSave" = false;
+          # "go.toolsManagement.autoUpdate" = false;
+          # "go.useLanguageServer" = true;
+          # "go.testFlags" = [ "-v" ];
+          # "go.coverOnSave" = false;
+          # "go.coverOnSingleTest" = true;
+          # "go.coverOnSingleTestFile" = true;
+          # "go.delveConfig" = {
+          #   "debugAdapter" = "dlv-dap";
+          #   "showGlobalVariables" = true;
+          # };
+          # "go.coverageDecorator" = {
+          #   "type" = "highlight";
+          #   "coveredHighlightColor" = "rgba(64,128,128,0.2)";
+          #   "uncoveredHighlightColor" = "rgba(128,64,64,0.2)";
+          #   "coveredBorderColor" = "rgba(64,128,128,0.4)";
+          #   "uncoveredBorderColor" = "rgba(128,64,64,0.4)";
+          # };
+
+          # "[python]" = {
+          #   "editor.codeActionsOnSave" = {
+          #     "source.organizeImports" = "explicit";
+          #   };
+          #   "editor.defaultFormatter" = "ms-python.python";
+          #   "editor.formatOnSave" = true;
+          # };
+          # "python.defaultInterpreterPath" = "${pkgs.python312}/bin/python3";
+
+          # "[rust]" = {
+          #   "editor.defaultFormatter" = "rust-lang.rust-analyzer";
+          #   "editor.formatOnSave" = true;
+          #   "editor.inlayHints.enabled" = "onUnlessPressed";
+          # };
+
+          # # Rust analyzer settings
+          # "rust-analyzer.cargo.features" = "all";
+          # "rust-analyzer.check.command" = "clippy";
+          # "rust-analyzer.check.extraArgs" = [ ];
+          # "rust-analyzer.completion.autoself.enable" = true;
+          # "rust-analyzer.files.excludeDirs" = [
+          #   "**/.git/**"
+          #   "**/target/**"
+          # ];
+          # "rust-analyzer.hover.actions.enable" = true;
+          # "rust-analyzer.inlayHints.parameterHints.enable" = true;
+          # "rust-analyzer.inlayHints.renderColons" = true;
+          # "rust-analyzer.inlayHints.typeHints.enable" = true;
+          # "rust-analyzer.lens.enable" = true;
+          # "rust-analyzer.lens.run.enable" = true;
+          # "rust-analyzer.lens.implementations.enable" = true;
+          # "rust-analyzer.lens.references.adt.enable" = true;
+          # "rust-analyzer.lens.references.method.enable" = true;
+          # "rust-analyzer.lens.references.trait.enable" = true;
 
           # Font settings
           "editor.fontFamily" = "'MesloLGMDZ Nerd Font', 'JetBrainsMono NF', 'FiraCode Nerd Font', monospace";
@@ -108,47 +192,12 @@
           "chat.editor.fontFamily" = "'MesloLGMDZ Nerd Font', 'JetBrainsMono NF', 'FiraCode Nerd Font', monospace";
           "debug.console.fontFamily" = "'MesloLGMDZ Nerd Font','JetBrainsMono NF', 'FiraCode Nerd Font', monospace";
           "debug.console.fontSize" = 20;
-          "terminal.integrated.fontFamily" = "'MesloLGMDZ Nerd Font', 'JetBrainsMono NF', 'FiraCode Nerd Font', monospace";
-          "terminal.integrated.fontSize" = 20;
 
-          # Update settings
-          "extensions.autoCheckUpdates" = false;
-          "update.mode" = "none";
-
-          # File settings
-          "files.associations" = {
-            "*.rs" = "rust";
-          };
-          "json.schemaDownload.enable" = false;
-          "window.openFilesInNewWindow" = "on";
-          "window.zoomLevel" = 1;
-
-          # Go settings
-          "go.lintTool" = "golangci-lint";
-          "go.testOnSave" = false;
-          "go.toolsManagement.autoUpdate" = false;
-          "go.useLanguageServer" = true;
-          "go.testFlags" = [ "-v" ];
-          "go.coverOnSave" = false;
-          "go.coverOnSingleTest" = true;
-          "go.coverOnSingleTestFile" = true;
-          "go.delveConfig" = {
-            "debugAdapter" = "dlv-dap";
-            "showGlobalVariables" = true;
-          };
-          "go.coverageDecorator" = {
-            "type" = "highlight";
-            "coveredHighlightColor" = "rgba(64,128,128,0.2)";
-            "uncoveredHighlightColor" = "rgba(128,64,64,0.2)";
-            "coveredBorderColor" = "rgba(64,128,128,0.4)";
-            "uncoveredBorderColor" = "rgba(128,64,64,0.4)";
-          };
-
-          # LLDB settings
-          "lldb.commandCompletions" = true;
-          "lldb.evaluateForHovers" = true;
-          "lldb.launch.terminal" = "integrated";
-          "lldb.suppressMissingSourceFiles" = true;
+          # # File settings
+          # "files.associations" = {
+          #   "*.rs" = "rust";
+          #   "*.go" = "go";
+          # };
 
           # Theme and icon settings
           "material-icon-theme.activeIconPack" = "nest";
@@ -157,51 +206,8 @@
           "material-icon-theme.hidesExplorerArrows" = true;
           "workbench.iconTheme" = "material-icon-theme";
           "workbench.productIconTheme" = "material-product-icons";
-          "workbench.colorTheme" = "Dracula";
-          "workbench.preferredDarkColorTheme" = "Dracula";
-
-          # Remote settings
-          "remote.SSH.configFile" = "~/.ssh/config";
-
-          # Coverage Settings
-          "coverage-gutters.coverageFileNames" = [
-            "target/coverage/lcov.info"
-          ];
-          "coverage-gutters.showGutterCoverage" = true;
-          "coverage-gutters.showLineCoverage" = true;
-          "coverage-gutters.showRulerCoverage" = true;
-          "coverage-gutters.highlightdark" = "rgba(64,128,64,0.4)";
-
-          # Rust analyzer settings
-          "rust-analyzer.cargo.features" = "all";
-          "rust-analyzer.check.command" = "clippy";
-          "rust-analyzer.check.extraArgs" = [ ];
-          "rust-analyzer.completion.autoself.enable" = true;
-          "rust-analyzer.files.excludeDirs" = [
-            "**/.git/**"
-            "**/target/**"
-          ];
-          "rust-analyzer.hover.actions.enable" = true;
-          "rust-analyzer.inlayHints.parameterHints.enable" = true;
-          "rust-analyzer.inlayHints.renderColons" = true;
-          "rust-analyzer.inlayHints.typeHints.enable" = true;
-          "rust-analyzer.lens.enable" = true;
-          "rust-analyzer.lens.run.enable" = true;
-          "rust-analyzer.lens.implementations.enable" = true;
-          "rust-analyzer.lens.references.adt.enable" = true;
-          "rust-analyzer.lens.references.method.enable" = true;
-          "rust-analyzer.lens.references.trait.enable" = true;
-
-          # Task settings
-          "task.allowAutomaticTasks" = "off";
-
-          # Terminal settings
-          "terminal.integrated.defaultProfile.osx" = "zsh";
-          "terminal.integrated.profiles.osx" = {
-            "zsh" = {
-              "path" = "${config.home.homeDirectory}/.nix-profile/bin/zsh";
-            };
-          };
+          "workbench.colorTheme" = "Dracula Theme";
+          "workbench.preferredDarkColorTheme" = "Dracula Theme";
 
           # Workbench color customizations
           "workbench.colorCustomizations" = {
